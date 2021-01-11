c429858479d87450a074b10d4a6e477abaedd07c
Alfie Fish <FIS202000097@longroad.ac.uk>
2021-01-11 14:35:59 +0000

    Add project files.

---
+++ hokemon/Hokemon.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hokemon
+{
+    class Hokemon
+    {
+        //hokemon attributes
+        private string name ;
+        private int health ;
+        private int maxHealth;
+        private int attack;
+        private int speed;
+        private int defence;
+
+        //below is a constructor method used to create an object
+        public Hokemon()
+        {
+            give_name();
+            maxHealth = int_generator(10,100);
+            health = maxHealth;
+            attack = int_generator(10, 100);
+            speed = int_generator(10, 100);
+            defence = int_generator(10, 100);
+
+            
+        }
+        public string getname()
+        {
+            return name;
+        }
+        public void get_details()
+        {
+            Console.WriteLine("health: {0}/{1} \n Attack: {2} \n Speed:{3} \n Defence: {4}", health, maxHealth, attack, speed, defence);
+        }
+    public void give_name()
+        {
+            Console.WriteLine("enter the name:");
+            name = Console.ReadLine();
+        }
+    public int int_generator(int minValue, int maxValue)
+        {
+            Random rnd = new Random();
+
+            int randomValue ;
+            randomValue = rnd.Next(minValue, maxValue);
+
+            return randomValue;
+        }
+    }
+
+}
---
+++ hokemon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to hokeworld home of the hokemon");
+
+                //instantiating a new object
+            Hokemon hoke01 = new Hokemon();
+
+            Console.WriteLine("the name is {0}",hoke01.getname());
+
+            hoke01.get_details();
+            Console.ReadLine();
+            Console.WriteLine("//////////////////////////////////////");
+            Hokemon hoke02 = new Hokemon();
+
+            Console.WriteLine("the name is {0}", hoke02.getname());
+
+            hoke02.get_details();
+            Console.ReadLine();
+        }
+    }
+}
